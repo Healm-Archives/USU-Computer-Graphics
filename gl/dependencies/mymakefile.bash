dac3a42bf02e9e9c7f238b56175bb35ac523a6c9
Healm <116085297+Agent-Healm@users.noreply.github.com>
2026-05-05 23:49:55 +0700

    update shell script for makefile to compile with llvm-mingw from github

--- gl/dependencies/mymakefile.bash
+++ gl/dependencies/mymakefile.bash
@@ -3,9 +3,10 @@
 
 # sudo apt install mingw-w64
 
-# Makefile.mingw (probably line 9)
+# Makefile.mingw (probably line 9) (this step is only for gcc?)
 # LDFLAGS.EXTRA += -nostdlib
 
+# build with gcc installed from apt
 make SYSTEM=linux-mingw64 \
      CC=x86_64-w64-mingw32-gcc \
      LD=x86_64-w64-mingw32-ld \
@@ -13,7 +14,32 @@ make SYSTEM=linux-mingw64 \
      RANLIB=x86_64-w64-mingw32-ranlib \
      STRIP=x86_64-w64-mingw32-strip
 
+# build with clang
+x86_64-w64-mingw32-clang \
+  -target x86_64-w64-windows-gnu \
+  --sysroot=/home/healm/susdo/toolchains/mingw \
+  -fuse-ld=lld \
+  -nostdlib \
+  -DGLEW_BUILD \
+  -Iinclude \
+  src/glew.c \
+  -shared \
+  -lopengl32 -lgdi32 -luser32 -lkernel32 \
+  -Wl,--out-implib,lib/libglew32.dll.a \
+  -o lib/libglew32.dll
+
+# build with gcc installed from llvm 
+x86_64-w64-mingw32-gcc \
+  -nostdlib \
+  -DGLEW_BUILD \
+  -Iinclude \
+  src/glew.c \
+  -shared \
+  -lopengl32 -lgdi32 -luser32 -lkernel32 \
+  -Wl,--out-implib,lib/libglew32.dll.a \
+  -o lib/libglew32.dll
+
 grab lib folder
      - glew32.dll (put in bin)
-     - libglew32.a (put in lib)
+     - libglew32.a (put in lib) (only for gcc)
      - libglew32.dll.a (put in lib)
